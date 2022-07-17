e0990df45a5ff0a640e4ccfc4be319773690b526
AnnaShazk <dream1nd.find@gmail.com>
2022-07-17 23:49:32 +0200

    test_3

--- assets/js/payment-block.js
+++ assets/js/payment-block.js
@@ -1,46 +1,98 @@
-"use sting"
+"use sting";
 
-let button = document.getElementsByClassName('pay_currency');
-let rec = document.getElementsByClassName('recalculations');
+let button = document.getElementsByClassName("pay_currency");
+let rec = document.getElementsByClassName("recalculations");
 
-for(let key in button){
+for (let key in button) {
+  button[0].style.backgroundImage =
+    "linear-gradient(195deg, #49a3f1 0%, #1A73E8 100%)";
+  button[0].style.color = "#fff";
 
-    button[0].style.backgroundImage = "linear-gradient(195deg, #49a3f1 0%, #1A73E8 100%)";
-    button[0].style.color = "#fff";
+  button[key].onclick = function () {
+    for (let i = 0; i < button.length; i++) {
+      button[i].style.backgroundImage = "none";
+      button[key].style.backgroundImage =
+        "linear-gradient(195deg, #49a3f1 0%, #1A73E8 100%)";
 
-    button[key].onclick = function(){
+      button[i].style.color = "#49a3f1";
+      button[key].style.color = "#fff";
 
-        for(let i =0;i<button.length;i++){
-          
-          button[i].style.backgroundImage = "none";
-          button[key].style.backgroundImage = "linear-gradient(195deg, #49a3f1 0%, #1A73E8 100%)";
+      for (let k = 0; k < rec.length; k++) {
+        rec[k].style.display = "none";
+      }
 
-          button[i].style.color = "#49a3f1";
-          button[key].style.color = "#fff";
-
-          for(let k=0;k<rec.length;k++){
-            rec[k].style.display= 'none';
-          }
-          
-          if(key>0){
-            rec[key-1].style.display= 'block';
-          }
-        }
+      if (key > 0) {
+        rec[key - 1].style.display = "block";
+      }
     }
-    
-
+  };
 }
 
-// let value = document.getElementById('usd_click');
-// value.onchange = function(){
-//   let cur = document.getElementsByClassName('currency');
-  
-//   if (window.getComputedStyle(cur[1]).display == 'block') {
-//     document.getElementsByClassName('recalculations')[0].style.display = 'block';
-//   } else if(window.getComputedStyle(cur[1]).display == 'none'){
-//     document.getElementsByClassName('recalculations')[0].style.display = 'none';
-//   }
-  
+// function copyFunction(copyButton) {
+//   copyButton.style.color = "blue";
+//   let copyText = document.getElementById("copy_text");
+
+//   let a = document.getElementById("copy_info");
+//   a.innerHTML = 'Текст скопійовано';
+
+//   /* Copy the text inside the text field */
+
+//   navigator.clipboard.writeText(copyText.innerHTML);
+
+//   let start = Date.now();
+
+//   let timer = setInterval(function () {
+//     let timePassed = Date.now() - start;
+
+//     if (timePassed > 2800) {
+//       copyButton.style.color = "grey";
+//       a.innerHTML = 'Скопіювати текст';
+//     }
+   
+
+//     if (timePassed > 3000) clearInterval(timer);
+//   });
 // }
+
+
+let copyButton = document.getElementsByClassName('copy_button');
+
+for (let key in copyButton){
+
+  copyButton[key].onclick = function(){
+
+    this.style.color = "#49a3f1";
+
+    let copyText= document.getElementsByClassName("copy_text");
+    navigator.clipboard.writeText(copyText[key].innerHTML);
+
+
+    let info = document.getElementsByClassName("copy_info");
+    info[key].innerHTML = 'Cкопійовано';
+
+  
+    let start = Date.now();
+  
+    let timer = setInterval(function () {
+      let timePassed = Date.now() - start;
+  
+      if (timePassed > 2800) {
+        copyButton[key].style.color = "grey";
+        info[key].innerHTML = 'Скопіювати';
+      }
+     
+  
+      if (timePassed > 3000) clearInterval(timer);
+    });
+
+  }
+  
+}
+
+
+
+
+
+
 
 
