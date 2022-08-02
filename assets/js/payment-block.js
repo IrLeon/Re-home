919af1a06879f3423adc7912d684e8c36b32f053
Tolik4umak <Tolik4umak@gmail.com>
2022-08-03 01:51:57 +0200

    payment_block_done_mobileCopiedButton

--- assets/js/payment-block.js
+++ assets/js/payment-block.js
@@ -38,28 +38,45 @@ let copyButton = document.getElementsByClassName('copy_button');
 for (let key in copyButton){
 
   copyButton[key].onclick = function(){
-
+// change color of "copy" button
     this.style.color = "#49a3f1";
 
+// Create copy of the text
     let copyText= document.getElementsByClassName("copy_text");
     navigator.clipboard.writeText(copyText[key].innerHTML);
 
-
+// Change text on title
     let info = document.getElementsByClassName("copy_info");
     if (info[key].innerHTML=='Copy'){
       info[key].innerHTML = 'Сopied';
     } else if(info[key].innerHTML=='Скопіювати'){
       info[key].innerHTML = 'Cкопійовано';
     }
-    
-  
+
+// Add mobile title      NEW   NEW   NEW   NEW   NEW
+    let copyInfoMob = document.getElementById('copy_info_mob');
+    copyInfoMob.style.display = 'block';
+    if (document.documentElement.clientWidth > 991) {
+      copyInfoMob.style.display = 'none';
+    }
+///////////////////////////////
+
+// Create animation
     let start = Date.now();
   
     let timer = setInterval(function () {
       let timePassed = Date.now() - start;
   
       if (timePassed > 2800) {
+        // change color of "copy" button back
         copyButton[key].style.color = "grey";
+
+        // Hide mobile title  NEW   NEW   NEW   NEW   NEW
+        copyInfoMob.style.display = 'none';
+        ////////////////////////
+
+        
+        // Change text on title
         if (info[key].innerHTML=='Сopied'){
           info[key].innerHTML = 'Copy';
         }else if(info[key].innerHTML=='Cкопійовано'){
